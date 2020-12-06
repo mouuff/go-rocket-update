b2a96962616f27cca5f8b93b0c10b2e56ed0651d
Arnaud Alies <arnaudalies.py@gmail.com>
2020-12-06 11:57:19 +0100

    Update helper_test.go

--- helper/helper_test.go
+++ helper/helper_test.go
@@ -52,3 +52,35 @@ func TestCopyFile(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func verifyChecksumFile(src, expectedChecksum string) error {
+	checksum, err := helper.ChecksumFile(src)
+	if err != nil {
+		return err
+	}
+	if checksum != expectedChecksum {
+		return fmt.Errorf("checksum: %s != expectedChecksum: %s", checksum, expectedChecksum)
+	}
+	return nil
+}
+
+func TestChecksum(t *testing.T) {
+	err := verifyChecksumFile(path.Join("testdata", "empty.txt"),
+		"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+	if err != nil {
+		t.Error(err)
+	}
+	err = verifyChecksumFile(path.Join("testdata", "TempleOS.ISO"),
+		"5d0fc944e5d89c155c0fc17c148646715bc1db6fa5750c0b913772cfec19ba26")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Test wrong checksum:
+	err = verifyChecksumFile(path.Join("testdata", "TempleOS.ISO"),
+		"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+	if err == nil {
+		t.Error(fmt.Errorf("verifyChecksumFile returned not nil on a bad checksum"))
+	}
+
+}
