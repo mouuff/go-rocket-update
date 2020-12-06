4009cae718c124e3ad469a0ef0b2108ba6919ac8
Arnaud Alies <arnaudalies.py@gmail.com>
2020-12-06 11:45:20 +0100

    Add helper functions

--- helper/helper.go
+++ helper/helper.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"io"
 	"os"
 )
@@ -25,4 +27,19 @@ func CopyFile(src string, dest string) error {
 	}
 	err = out.Sync()
 	return err
+}
+
+func ChecksumFile(src string) (string, error) {
+	f, err := os.Open(src)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
--- helper/helper_test.go
+++ helper/helper_test.go
@@ -2,6 +2,7 @@ package helper_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"testing"
@@ -9,13 +10,45 @@ import (
 	"github.com/mouuff/easy-update/helper"
 )
 
+func copyAndChecksumFile(src string) error {
+	dir, err := ioutil.TempDir("", "copyAndChecksumFile")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir)
+
+	dest := path.Join(dir, "dest.txt")
+	err = helper.CopyFile(src, dest)
+	if err != nil {
+		return err
+	}
+
+	srcChecksum, err := helper.ChecksumFile(src)
+	if err != nil {
+		return err
+	}
+	destChecksum, err := helper.ChecksumFile(dest)
+	if err != nil {
+		return err
+	}
+	if destChecksum != srcChecksum {
+		return fmt.Errorf("destChecksum: %s != srcChecksum: %s", dest, src)
+	}
+
+	return nil
+}
+
 func TestCopyFile(t *testing.T) {
-
-	dir := os.TempDir()
-
-	fmt.Print(dir)
-
-	helper.CopyFile(path.Join("testdata", "small.exe"), path.Join(dir, "small2.txt"))
-	t.Error(dir)
-	//defer os.RemoveAll(dir)
+	err := copyAndChecksumFile(path.Join("testdata", "small.exe"))
+	if err != nil {
+		t.Error(err)
+	}
+	err = copyAndChecksumFile(path.Join("testdata", "TempleOS.ISO"))
+	if err != nil {
+		t.Error(err)
+	}
+	err = copyAndChecksumFile(path.Join("testdata", "empty.txt"))
+	if err != nil {
+		t.Error(err)
+	}
 }
