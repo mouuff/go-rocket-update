80715f55b96aa5e648b0902eb9aea46732d5e5ca
Arnaud Alies <arnaudalies.py@gmail.com>
2020-12-11 22:23:33 +0100

    Update updater_folder.go

--- updater/updater_folder.go
+++ updater/updater_folder.go
@@ -1,11 +1,25 @@
-package fileio_test
+package updater
 
 import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+
 	"github.com/mouuff/easy-update/provider"
 )
 
 type UpdaterFolder struct {
-	provider provider.Provider
+	provider      provider.Provider
+	projectFolder string
+}
+
+func NewUpdaterFolder() *UpdaterFolder {
+	return &UpdaterFolder{}
+}
+
+func (u *UpdaterFolder) SetProjectFolder(projectFolder string) {
+	u.projectFolder = projectFolder
 }
 
 func (u *UpdaterFolder) SetProvider(p provider.Provider) {
@@ -13,5 +27,24 @@ func (u *UpdaterFolder) SetProvider(p provider.Provider) {
 }
 
 func (u *UpdaterFolder) Run() error {
-	return nil
+
+	err := u.provider.Walk(func(relPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", relPath, err)
+			return err
+		}
+		destPath := path.Join(u.projectFolder, relPath)
+		if info.IsDir() {
+			os.MkdirAll(destPath, os.ModePerm)
+		} else {
+			os.MkdirAll(filepath.Dir(destPath), os.ModePerm)
+			err = u.provider.Retrieve(relPath, destPath)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return err
 }
