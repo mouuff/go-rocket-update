0db7c62bf677a7da8fc241d9fba42c455cb7531a
Arnaud Alies <arnaudalies.py@gmail.com>
2020-12-28 14:59:15 +0100

    Update keygen.go

--- command/keygen.go
+++ command/keygen.go
@@ -1,10 +1,13 @@
 package command
 
 import (
+	"errors"
 	"flag"
 	"io/ioutil"
+	"log"
 
 	"github.com/mouuff/go-rocket-update/crypto"
+	"github.com/mouuff/go-rocket-update/fileio"
 )
 
 type Keygen struct {
@@ -26,25 +29,41 @@ func (cmd *Keygen) Init(args []string) error {
 }
 
 func (cmd *Keygen) Run() error {
+
+	privKeyPath := cmd.keyName
+	pubKeyPath := cmd.keyName + ".pub"
+
+	if fileio.FileExists(privKeyPath) {
+		return errors.New("Key '" + privKeyPath + "' already exists")
+	}
+
+	if fileio.FileExists(pubKeyPath) {
+		return errors.New("Key '" + pubKeyPath + "' already exists")
+	}
+
+	log.Println("Generating keys...")
 	priv, err := crypto.GeneratePrivateKey()
 	if err != nil {
 		return err
 	}
 
 	privPem := crypto.ExportPrivateKeyAsPem(priv)
-	err = ioutil.WriteFile(cmd.keyName, privPem, 0600)
+	err = ioutil.WriteFile(privKeyPath, privPem, 0600)
 	if err != nil {
 		return err
 	}
+	log.Println("Created private key: " + privKeyPath)
 
 	pubPem, err := crypto.ExportPublicKeyAsPem(&priv.PublicKey)
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(cmd.keyName+".pub", pubPem, 0644)
+	err = ioutil.WriteFile(pubKeyPath, pubPem, 0644)
 	if err != nil {
 		return err
 	}
+
+	log.Println("Created public key: " + pubKeyPath)
 
 	return nil
 }
