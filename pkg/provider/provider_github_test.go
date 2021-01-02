b64ef01d7039fb03ea8c6529e8211ac53cafe904
Arnaud Alies <arnaudalies.py@gmail.com>
2021-01-02 11:50:38 +0100

    Delete travis & stop skipping test provider github (now it should pass using github actions)

--- pkg/provider/provider_github_test.go
+++ pkg/provider/provider_github_test.go
@@ -1,7 +1,6 @@
 package provider_test
 
 import (
-	"os"
 	"runtime"
 	"testing"
 
@@ -9,10 +8,6 @@ import (
 )
 
 func TestProviderGithub(t *testing.T) {
-	if os.Getenv("CI") != "" {
-		// We skip this test because using travis the request to github often fails with a "HTTP 429 Too Many Requests" error
-		t.Skip("Skipping testing in CI environment")
-	}
 	p := &provider.Github{
 		RepositoryURL: "github.com/mouuff/go-rocket-update-example",
 		ZipName:       "binaries_" + runtime.GOOS + ".zip",
