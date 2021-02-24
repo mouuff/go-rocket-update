bf8b8c9f0586a0be37256afdeb29bf9db7904cc9
Arnaud Alies <arnaudalies.py@gmail.com>
2021-02-24 23:49:25 +0100

    Add TestGlobNewestFile

--- pkg/provider/common_test.go
+++ pkg/provider/common_test.go
@@ -1,6 +1,7 @@
 package provider_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	provider "github.com/mouuff/go-rocket-update/pkg/provider"
@@ -18,5 +19,20 @@ func TestGetLatestVersionFromPath(t *testing.T) {
 	version, err = provider.GetLatestVersionFromPath("binaries-v.zip")
 	if err == nil {
 		t.Error("Should return an error")
+	}
+}
+
+func TestGlobNewestFile(t *testing.T) {
+	match, err := provider.GlobNewestFile(filepath.Join("testdata", "Allum1-v*.tar.gz"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if filepath.Base(match) != "Allum1-v1.1.0.tar.gz" {
+		t.Error("Expected Allum1-v1.1.0.tar.gz")
+	}
+
+	match, err = provider.GlobNewestFile(filepath.Join("testdata", "doesntexists"))
+	if err == nil {
+		t.Error("Should return an error if file doesn't exists")
 	}
 }
