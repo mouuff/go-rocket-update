5c5191c74ccea93d351ae5acf6b94f14442f18bd
Arnaud Alies <arnaudalies.py@gmail.com>
2021-03-21 19:21:22 +0100

    Update provider_local.go

--- pkg/provider/provider_local.go
+++ pkg/provider/provider_local.go
@@ -39,17 +39,18 @@ func (c *Local) GetLatestVersion() (string, error) {
 // Walk walks all the files provided
 func (c *Local) Walk(walkFn WalkFunc) error {
 	return filepath.Walk(c.Path, func(filePath string, info os.FileInfo, walkErr error) error {
-		if walkErr == nil {
-			relPath, err := filepath.Rel(c.Path, filePath)
-			if err != nil {
-				return err
-			}
-			return walkFn(&FileInfo{
-				Path: relPath,
-				Mode: info.Mode(),
-			})
+		if walkErr != nil {
+			// TODO log walkErr ?
+			return nil // Ignore error and continue walking
 		}
-		return nil
+		relPath, err := filepath.Rel(c.Path, filePath)
+		if err != nil {
+			return err
+		}
+		return walkFn(&FileInfo{
+			Path: relPath,
+			Mode: info.Mode(),
+		})
 	})
 }
 
