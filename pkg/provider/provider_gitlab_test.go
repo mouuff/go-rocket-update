9dba8352b7ab0f215d4eda7c33f5398624536621
Julien Noblet <julien.noblet+github@gmail.com>
2023-02-09 18:27:49 +0100

    Add ApiURI for gitlab provider (#27)
    * Add ApiURI for gitlab provider
    it can let us using private server
    
    * Increase code coverage for provider_gitlab.go
    
    ---------
    
    Co-authored-by: Julien NOBLET <julien.noblet@gls-france.com>

--- pkg/provider/provider_gitlab_test.go
+++ pkg/provider/provider_gitlab_test.go
@@ -24,11 +24,37 @@ func TestProviderGitlab(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	pp := &provider.Gitlab{
+		ProjectID:   24021648,
+		ArchiveName: fmt.Sprintf("binaries_%s.zip", runtime.GOOS),
+		ApiURI:      "https://gitlab.com/api/v4/projects/%d/releases", // same as original
+	}
+
+	if err := pp.Open(); err != nil {
+		t.Fatal(err)
+	}
+	defer pp.Close()
+
+	err = ProviderTestWalkAndRetrieve(pp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	badProvider := &provider.Gitlab{
 		ProjectID:   424242424242424242,
 		ArchiveName: fmt.Sprintf("binaries_%s.zip", runtime.GOOS),
 	}
 	err = ProviderTestUnavailable(badProvider)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	badProviderApi := &provider.Gitlab{
+		ProjectID:   24021648,
+		ArchiveName: fmt.Sprintf("binaries_%s.zip", runtime.GOOS),
+		ApiURI:      "https://bad.url/api/%d/R",
+	}
+	err = ProviderTestUnavailable(badProviderApi)
 	if err != nil {
 		t.Fatal(err)
 	}
